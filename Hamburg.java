5eca3dc49e08e62df8764b7bf1ac7363d57c1a24
Lemuel <lemuel0208@gmail.com>
2023-01-31 11:12:18 +0100

    haha

--- Hamburg.java
+++ Hamburg.java
@@ -69,47 +69,47 @@ public class Hamburg
         bergedorf = new BBergedorf(); ev.verwaltungsListeElementeEintragen(bergedorf);
         wandsbek = new BWandsbek(); ev.verwaltungsListeElementeEintragen(wandsbek);
         
-        wind = new Wind(); ev.verwaltungsListeElementeEintragen(wind);
+        wind = new Wind("kraftwerkDetails.txt", "HMitte"); ev.verwaltungsListeElementeEintragen(wind);
         wind.bewegeHorizontal(325);
         wind.bewegeVertikal(425);
         
-        wind1 = new Wind(); ev.verwaltungsListeElementeEintragen(wind1);
+        wind1 = new Wind("kraftwerkDetails.txt", "HMitte"); ev.verwaltungsListeElementeEintragen(wind1);
         wind1.bewegeHorizontal(575);
         wind1.bewegeVertikal(450);
         
-        wind2 = new Wind(); ev.verwaltungsListeElementeEintragen(wind2);
+        wind2 = new Wind("kraftwerkDetails.txt", "HMitte"); ev.verwaltungsListeElementeEintragen(wind2);
         wind2.bewegeHorizontal(366);
         wind2.bewegeVertikal(342);
         
-        wind3 = new Wind(); ev.verwaltungsListeElementeEintragen(wind3);
+        wind3 = new Wind("kraftwerkDetails.txt", "HMitte"); ev.verwaltungsListeElementeEintragen(wind3);
         wind3.bewegeHorizontal(385);
         wind3.bewegeVertikal(355);
         
-        kohle = new Kohle(); ev.verwaltungsListeElementeEintragen(kohle);
+        kohle = new Kohle("kraftwerkDetails.txt", "HMitte"); ev.verwaltungsListeElementeEintragen(kohle);
         kohle.bewegeHorizontal(465);
         kohle.bewegeVertikal(400);
         
-        kohle2 = new Kohle(); ev.verwaltungsListeElementeEintragen(kohle2);
+        kohle2 = new Kohle("kraftwerkDetails.txt", "HMitte"); ev.verwaltungsListeElementeEintragen(kohle2);
         kohle2.bewegeHorizontal(100);
         kohle2.bewegeVertikal(240);
         
-        solar = new Solar(); ev.verwaltungsListeElementeEintragen(solar);
+        solar = new Solar("kraftwerkDetails.txt", "HMitte"); ev.verwaltungsListeElementeEintragen(solar);
         solar.bewegeHorizontal(370);
         solar.bewegeVertikal(300);
         
-        solar1 = new Solar(); ev.verwaltungsListeElementeEintragen(solar1);
+        solar1 = new Solar("kraftwerkDetails.txt", "HMitte"); ev.verwaltungsListeElementeEintragen(solar1);
         solar1.bewegeHorizontal(385);
         solar1.bewegeVertikal(285);
         
-        solar2 = new Solar(); ev.verwaltungsListeElementeEintragen(solar2);
+        solar2 = new Solar("kraftwerkDetails.txt", "HMitte"); ev.verwaltungsListeElementeEintragen(solar2);
         solar2.bewegeHorizontal(390);
         solar2.bewegeVertikal(365);
         
-        atom = new Atom(); ev.verwaltungsListeElementeEintragen(atom);
+        atom = new Atom("kraftwerkDetails.txt", "HMitte"); ev.verwaltungsListeElementeEintragen(atom);
         atom.bewegeHorizontal(500);
         atom.bewegeVertikal(200);
         
-        wasser = new Wasser(); ev.verwaltungsListeElementeEintragen(wasser);
+        wasser = new Wasser("kraftwerkDetails.txt", "HMitte"); ev.verwaltungsListeElementeEintragen(wasser);
         wasser.bewegeHorizontal(200);
         wasser.bewegeVertikal(300);
     }
